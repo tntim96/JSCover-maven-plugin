4363d8d344e780c452235e30ab5ad9ab221aeae1
tntim96 <tntim96@gmail.com>
2014-05-27 18:58:18 +1000

    Try CI with Chrome

--- plugin-parent/server/src/test/java/jscover/maven/ServerMojoTest.java
+++ plugin-parent/server/src/test/java/jscover/maven/ServerMojoTest.java
@@ -35,6 +35,7 @@ public class ServerMojoTest {
         ReflectionUtils.setVariableValueInObject(mojo, "lineCoverageMinimum", 66);
         ReflectionUtils.setVariableValueInObject(mojo, "branchCoverageMinimum", 66);
         ReflectionUtils.setVariableValueInObject(mojo, "functionCoverageMinimum", 66);
+        ReflectionUtils.setVariableValueInObject(mojo, "webDriverClassName", "org.openqa.selenium.chrome.ChromeDriver");
         //ReflectionUtils.setVariableValueInObject(mojo, "webDriverClassName", "org.openqa.selenium.firefox.FirefoxDriver");
     }
 
