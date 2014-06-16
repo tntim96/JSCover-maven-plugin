3d2c1e5008c75e9000a4f152cd6f5338010dbe56
tntim96 <tntim96@gmail.com>
2014-06-17 08:04:51 +1000

    Add tests

--- plugin-parent/common/src/test/java/jscover/maven/JSCoverMojoTest.java
+++ plugin-parent/common/src/test/java/jscover/maven/JSCoverMojoTest.java
@@ -1,13 +1,18 @@
 package jscover.maven;
 
+import jscover.ConfigurationCommon;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.codehaus.plexus.util.ReflectionUtils;
 import org.junit.Test;
+import org.mozilla.javascript.Context;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import static jscover.ConfigurationCommon.NO_INSTRUMENT_PREFIX;
+import static jscover.ConfigurationCommon.NO_INSTRUMENT_REG_PREFIX;
 import static jscover.maven.TestType.Custom;
 import static jscover.maven.TestType.QUnit;
 import static org.hamcrest.CoreMatchers.*;
@@ -51,10 +56,54 @@ public class JSCoverMojoTest {
     @Test
     public void shouldSetSystemProperties() throws Exception {
         Properties properties = new Properties();
-        properties.setProperty("a","b");
+        properties.setProperty("a", "b");
         ReflectionUtils.setVariableValueInObject(mojo, "systemProperties", properties);
         mojo.setSystemProperties();
         assertThat(System.getProperty("a"), equalTo("b"));
+    }
+
+    @Test
+    public void shouldSetCommonConfigurationDefaults() throws Exception {
+        ConfigurationCommon config = new ConfigurationCommon();
+
+        mojo.setCommonConfiguration(config);
+
+        assertThat(config.getJSVersion(), equalTo(Context.VERSION_1_5));
+        assertThat(config.isIncludeBranch(), equalTo(true));
+        assertThat(config.isIncludeFunction(), equalTo(true));
+    }
+
+    @Test
+    public void shouldSetCommonConfiguration() throws Exception {
+        ConfigurationCommon config = new ConfigurationCommon();
+        ReflectionUtils.setVariableValueInObject(mojo, "JSVersion", 180);
+        ReflectionUtils.setVariableValueInObject(mojo, "includeBranch", false);
+        ReflectionUtils.setVariableValueInObject(mojo, "includeBranch", false);
+        ReflectionUtils.setVariableValueInObject(mojo, "includeFunction", false);
+
+        mojo.setCommonConfiguration(config);
+
+        assertThat(config.getJSVersion(), equalTo(Context.VERSION_1_8));
+        assertThat(config.isIncludeBranch(), equalTo(false));
+        assertThat(config.isIncludeFunction(), equalTo(false));
+        assertThat(config.skipInstrumentation("include.js"), equalTo(false));
+        assertThat(config.skipInstrumentation("exclude/file.js"), equalTo(false));
+        assertThat(config.skipInstrumentation("exclude-reg/file.js"), equalTo(false));
+    }
+
+    @Test
+    public void shouldInterpretCommonConfigurationPathsCorrectly() throws Exception {
+        ConfigurationCommon config = new ConfigurationCommon();
+        List<String> instrumentPathArgs = new ArrayList<String>();
+        instrumentPathArgs.add(NO_INSTRUMENT_PREFIX+"/exclude/");
+        instrumentPathArgs.add(NO_INSTRUMENT_REG_PREFIX+"/exclude-.*/.*.js");
+        ReflectionUtils.setVariableValueInObject(mojo, "instrumentPathArgs", instrumentPathArgs);
+
+        mojo.setCommonConfiguration(config);
+
+        assertThat(config.skipInstrumentation("include.js"), equalTo(false));
+        assertThat(config.skipInstrumentation("exclude/file.js"), equalTo(true));
+        assertThat(config.skipInstrumentation("exclude-reg/file.js"), equalTo(true));
     }
 
     @Test
