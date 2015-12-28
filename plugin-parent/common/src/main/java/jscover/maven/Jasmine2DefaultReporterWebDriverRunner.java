56651b3d9c68a0fa2b82be21eafab368c20ca06c
tntim96 <tntim96@gmail.com>
2015-12-28 20:40:07 +1100

    Upgrade Jasmine 2.3.4->2.4.1. Fix old Jasmine support

--- plugin-parent/common/src/main/java/jscover/maven/Jasmine2DefaultReporterWebDriverRunner.java
+++ plugin-parent/common/src/main/java/jscover/maven/Jasmine2DefaultReporterWebDriverRunner.java
@@ -1,16 +1,24 @@
 package jscover.maven;
 
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static java.lang.String.format;
 
-public class Jasmine2DefaultReporterWebDriverRunner extends JasmineHtmlReporterWebDriverRunner {
+public class Jasmine2DefaultReporterWebDriverRunner extends JasmineWebDriverRunner {
+    public void waitForTestsToComplete(WebDriver webClient) throws MojoExecutionException {
+        new WebDriverWait(webClient, timeOutSeconds).until(ExpectedConditions.presenceOfElementLocated(By.className("jasmine-duration")));
+        new WebDriverWait(webClient, timeOutSeconds).until(ExpectedConditions.textToBePresentInElementLocated(By.className("jasmine-duration"), "finished"));
+    }
+
     public void verifyTestsPassed(WebDriver webClient) throws MojoFailureException {
         if (webClient.findElements(By.className("jasmine-failed")).size() != 0) {
             for (String failure : getFailures(webClient))
